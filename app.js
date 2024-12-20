05ad57e3aff2fcc4cf935242d0333d39e17a3dd0
aashishrautela <asisrautela612@gmail.com>
2024-12-20 12:42:24 +0530

    husky test

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-function sayHello(name){console.log("Hello, "+name+"!")}
+function sayHello(name) { console.log("Hello, " + name + "!") }
 
-const numbers=[1,2,3,4,5];numbers.forEach((num)=>{console.log(num*num)})
+const numbers = [1, 2, 3, 4, 5]; numbers.forEach((num) => { console.log(num * num) })
 
-let person={name:"John",age:30,job:"Developer"};function displayPersonInfo(person){console.log(person.name+" is a "+person.age+" year old "+person.job)}
+let person = { name: "John", age: 30, job: "Developer" }; function displayPersonInfo(person) { console.log(person.name + " is a " + person.age + " year old " + person.job) }
 
 displayPersonInfo(person)
 
-function add(a,b){return a+b}const result=add(5,7);console.log("The result is: "+result)
+function add(a, b) { return a + b } const result = add(5, 7); console.log("The result is: " + result)
